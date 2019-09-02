7dcbaba4ff1afe3828ad6f3bdec4c8367be1d1f7
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-09-02 10:22:25 -0400

    added AIC selection stuff

--- src/ploidy_model.py
+++ src/ploidy_model.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import thinned_poisson_mixture_model as tp
 
 
@@ -13,3 +13,20 @@ def ploidy_Likelihood(x, n, lam):
 def weighted_Ploidy_Log_Likelihood(lh):
     w = tp.get_Weights(lh)
     return np.sum(np.log(np.sum(np.multiply(lh, w[:, np.newaxis]), axis = 0)))
+
+
+# Calculates the  Akaike Information Criterion (AIC) value of x when given a
+# list of ploidy models. Returns a tuple with the log likelihood values and the
+# AIC values
+def get_Log_Likelihood_AIC(x, models, lam):
+    w_lh = np.zeros(np.shape(models))
+    k = np.floor(models / 2) + 2
+    for i in range(models):
+        w_lh[i] = weighted_Ploidy_Log_Likelihood(
+            ploidy_Likelihood(x, models[i], lam))
+    return (w_lh, (2 * k) - (2 * w_lh))
+
+
+# selects best model based on the AIC value
+def select_model(aic, model):
+    return model[np.argmin(aic)]
