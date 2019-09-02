d0f3b5ec6475e8bdd0c649dc23269a58452e8488
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-09-02 14:58:34 -0400

    added more functionality to main file

--- src/ploidr.py
+++ src/ploidr.py
@@ -1,4 +1,7 @@
 import argparse
+import numpy as np
+import process_bam as pb
+import ploidy_model as pm
 
 
 if __name__ == '__main__':
@@ -8,12 +11,12 @@ if __name__ == '__main__':
     process_bam = subparsers.add_parser("process_bam")
     process_bam.add_argument("--bam", required = True)
     process_bam.add_argument("--out", required = True)
-
+    process_bam.add_argument("--bed", default = False)
 
     denoise = subparsers.add_parser("denoise")
     denoise.add_argument("--count_file", required = True)
-    denoise.add_argument("--out")
-    denoise.add_argument("iter", type = int)
+    denoise.add_argument("--out", required = True)
+    denoise.add_argument("iter", type = int, default = 3, required = True)
 
     histo = subparsers.add_parser("histo")
     histo.add_argument("--count_file", required = True)
@@ -24,3 +27,24 @@ if __name__ == '__main__':
     assess.add_argument("--ploidies", nargs='+', type = int, required = True)
 
     args = parser.parse_args()
+
+    if args.subparser == 'process_bam':
+        subargs = process_bam.parse_args()
+        print("Now processing BAM file %s..." % subargs.bam)
+        if subargs.bed:
+            print("\tprocessing subset found in %s" % subargs.bed)
+        pb.get_biallelic_coverage(subargs.bam, subargs.out, subargs.bed)
+        print("Success!")
+    elif args.subparser == 'denoise':
+        subargs = denoise.parse_args()
+        print("Denoising count file %s..." % subargs.count_file)
+        np.savetxt(subargs.out, pb.denoise_reads(subargs.count_file,
+                                                 subargs.iter),
+                   fmt='%d')
+        print("Filtered data sucessfully saved in %s" % subargs.out)
+    elif args.subparser == 'histo':
+        # FILL IN TODO
+        None
+    elif args.subparser == 'assess':
+       # FILL IN TODO
+       None
