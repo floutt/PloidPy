0b96817e462ed9adaefc86587582ee60ba4a263d
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-09-03 21:17:50 -0400

    changed label for new binomail model

--- src/ploidy_model.py
+++ src/ploidy_model.py
@@ -1,29 +1,30 @@
 import numpy as np
-import thinned_poisson_mixture_model as tp
+import binom_model as bm
 
 
 # calculates the likelihood values for each heterozygous state for the x matrix
 # for the n ploidy model. This model does NOT calculate the WEIGHTED
 # likelihood, just the likelihood of each value for each model.
-def ploidy_Likelihood(x, n, lam):
+def ploidy_Likelihood(x, n, dens, n_val):
     het_p = np.arange(1, np.floor(n/2) + 1) / n
-    return tp.get_Likelihood(x, het_p, lam)
+    return bm.get_Likelihood(x, het_p, dens, n_val)
 
 
 def weighted_Ploidy_Log_Likelihood(lh):
-    w = tp.get_Weights(lh)
+    w = bm.get_Weights(lh)
+    a = np.multiply(lh, w[:, np.newaxis])
     return np.sum(np.log(np.sum(np.multiply(lh, w[:, np.newaxis]), axis = 0)))
 
 
 # Calculates the  Akaike Information Criterion (AIC) value of x when given a
 # list of ploidy models. Returns a tuple with the log likelihood values and the
 # AIC values
-def get_Log_Likelihood_AIC(x, models, lam):
+def get_Log_Likelihood_AIC(x, models, dens, n_val):
     w_lh = np.zeros(np.shape(models))
     k = np.floor(models / 2) + 2
     for i in range(len(models)):
         w_lh[i] = weighted_Ploidy_Log_Likelihood(
-            ploidy_Likelihood(x, models[i], lam))
+            ploidy_Likelihood(x, models[i], dens, n_val))
     return (w_lh, (2 * k) - (2 * w_lh))
 
 
