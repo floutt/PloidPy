5243f57781d0a917c5be80afcb08e55b66d33896
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-09-19 09:57:29 -0400

    generalized counter

--- src/process_bam.py
+++ src/process_bam.py
@@ -45,7 +45,9 @@ def get_biallelic_coverage(bamfile, outfile, bed = False, quality = 15):
         # categorize sites by unique nucleotide count
         pop_sites = np.sum((nuc_cov == 0) == False, axis = 0)
         site_counts = np.unique(pop_sites, return_counts = True)
-        counts += site_counts[1][np.where(site_counts[0] > 0)[0]]
+        for i, count in enumerate(site_count[0]):
+            if count != 0:
+                counts[count-1] += site_counts[1][i]
 
         # only get biallelic site
         nuc_cov = nuc_cov[:, pop_sites == 2].T
