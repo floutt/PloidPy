fixed some precision issues

src/ploidr.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         print("Files saved in %s.hexbin.pdf and %s.histo.pdf!" % (args.count_file, args.count_file))
     elif args.subparser == 'assess':
         cnts = np.loadtxt(args.count_file)
-        r, p_nb = nb.fit_nbinom(x[:,1])
+        r, p_nb = nb.fit_nbinom(cnts[:,1])
         pld = np.array(args.ploidies)
         aicllh = pm.get_Log_Likelihood_AIC(cnts, pld, r, p_nb)
         print(aicllh[0])

src/ploidy_model.py
@@ -2,6 +2,9 @@ import numpy as np
 import binom_model as bm
 
 
+EPS = np.finfo(np.float64).tiny
+
+
 # calculates the likelihood values for each heterozygous state for the x matrix
 # for the n ploidy model. This model does NOT calculate the WEIGHTED
 # likelihood, just the likelihood of each value for each model.
@@ -11,9 +14,11 @@ def ploidy_Likelihood(x, n, r, p_nb):
 
 
 def weighted_Ploidy_Log_Likelihood(lh):
-    w = bm.get_Weights(lh)
-    a = np.multiply(lh, w[:, np.newaxis])
-    return np.sum(np.log(np.sum(np.multiply(lh, w[:, np.newaxis]), axis = 0)))
+    lh0 = lh.copy()
+    lh0[lh0 == 0] = EPS
+    w = bm.get_Weights(lh0)
+    a = np.multiply(lh0, w[:, np.newaxis])
+    return np.sum(np.log(np.sum(np.multiply(lh0, w[:, np.newaxis]), axis = 0)))
 
 
 # Calculates the  Akaike Information Criterion (AIC) value of x when given a