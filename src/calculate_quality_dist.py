6469476607ea32c352449313d5997089c54e3f41
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-10-02 14:50:31 -0400

    adjusted implementation of error distribution system. No longer uses an intermediate file format

--- src/calculate_quality_dist.py
+++ src/calculate_quality_dist.py
@@ -1,18 +1,17 @@
 import argparse
 import sys
-import os
+import pysam
 
 
-def avg_quality(fbuf, out):
+def avg_quality(bamfile, out, quality):
     nmrtr = {}  # numerator for mean calculation
     denom = {}  # denominator for mean calculation
-    for line in fbuf:
-        lsplt = line.split("\t")
-        cov = int(lsplt[3])
+    bam = pysam.AlignmentFile(bamfile, "rb")
+    for pcol in bam.pileup(min_base_quality = quality):
+        qual = 10 ** -(np.array(pcol.get_mapping_qualities())/10)
+        cov = len(qual)
         if cov < 2:
             continue
-        qstr = list(filter(lambda x: not (x in [">", "<", "^", "~", "*"]), lsplt[5]))
-        qual = list(map(lambda x: 10 ** (-(ord(x))/10), qstr))
         if cov not in nmrtr:
             nmrtr[cov] = sum(qual)
             denom[cov] = len(qual)
@@ -29,32 +28,15 @@ def avg_quality(fbuf, out):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--bam")
-    group.add_argument("--pileup")
+    parser.add_argument("--bam")
     parser.add_argument("--out")
     parser.add_argument("--quality", type = int, default = 15)
     args = parser.parse_args()
 
-    tmp = "tmp/mpu"
     outbuf = None
-    pufbuf = None
-
     if args.out:
         outbuf = open(args.out, "w+")
     else:
         outbuf = sys.stdout
 
-    if args.pileup:
-        pufbuf = open(args.pileup)
-    elif args.bam:
-        os.system("mkdir -p tmp")
-        os.system("samtools mpileup -Q %s -q 0 -A %s > %s" % (args.quality, args.bam, tmp))
-        pufbuf = open(tmp)
-    else:
-        pufbuf = sys.stdin
-
-    avg_quality(pufbuf, outbuf)
-
-    if args.bam:
-        os.system("rm %s" % tmp)
+    avg_quality(pufbuf, outbuf, args.quality)
