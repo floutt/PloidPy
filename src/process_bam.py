46390a6fcdfeaecda0e28c4cc7e8391a26a84b3a
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2019-11-04 20:51:47 -0500

    speed improvements

--- src/process_bam.py
+++ src/process_bam.py
@@ -2,7 +2,7 @@ import numpy as np
 import pysam
 import os
 import scipy.stats as stats
-
+import time
 
 # in the case that a value produces a probability of 0 (or a probability lower
 # than what can be represented with floating-point numbers, we replace it with
@@ -17,32 +17,36 @@ def get_biallelic_coverage(bamfile, outfile, bed = False, map_quality = 15):
     out = open(outfile, "w+")
     qual_num = 0
     qual_dnm = 0
-    nuc_map = {"a":0, "t":1, "g":2, "c":3}
+    nuc_map = {"a":0, "t":1, "g":2, "c":3, "A":0, "T":1, "G":2, "C":3}
+
+    def gt_0(x):
+        return x > 0
 
     def pcol_iter(pcol):
-        ATGC = np.zeros(4)
+        ATGC = [0, 0, 0, 0]
         bases = pcol.get_query_sequences()
         for b in bases:
             if b == '': continue
-            ATGC[nuc_map[b.lower()]] += 1
-        ATGC = ATGC[ATGC > 0]
+            ATGC[nuc_map[b]] += 1
+        ATGC = list(filter(gt_0, ATGC))
         if not len(ATGC) == 2:
-            return
-        out.write("%d %d\n" % (np.min(ATGC), np.sum(ATGC)))
+            return False
+        out.write("%d %d\n" % (min(ATGC), sum(ATGC)))
+        return True
 
     if bed:
         f = open(bed)
         for line in f:
             br = line.split()
             for pcol in bam.pileup(br[0], int(br[1]), int(br[2]), min_base_quality = map_quality):
-                pcol_iter(pcol)
-                qual_num += np.sum(10 ** -(np.array(pcol.get_query_qualities())/10))
-                qual_dnm += pcol.get_num_aligned()
+                if pcol_iter(pcol):
+                    qual_num += sum(map(lambda x: 10 ** -(x/10), pcol.get_query_qualities()))
+                    qual_dnm += pcol.get_num_aligned()
     else:
         for pcol in bam.pileup(min_base_quality = map_quality):
-            pcol_iter(pcol)
-            qual_num += np.sum(10 ** -(np.array(pcol.get_query_qualities())/10))
-            qual_dnm += pcol.get_num_aligned()
+            if pcol_iter(pcol):
+                qual_num += sum(map(lambda x: 10 ** -(x/10), pcol.get_query_qualities()))
+                qual_dnm += pcol.get_num_aligned()
     print("Base quality error probability:")
     print(qual_num/qual_dnm)
     out.close()
