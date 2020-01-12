52d965b8e0d02eee6e046389273cee6f8b307e83
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2020-01-12 15:30:23 -0500

    added important count feature

--- PloidPy/process_bam.py
+++ PloidPy/process_bam.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pysam
-import os
 import scipy.stats as stats
-import time
 
 # in the case that a value produces a probability of 0 (or a probability lower
 # than what can be represented with floating-point numbers, we replace it with
@@ -12,12 +10,13 @@ import time
 EPS = np.finfo(np.float64).tiny
 
 
-def get_biallelic_coverage(bamfile, outfile, bed = False, map_quality = 15):
+def get_biallelic_coverage(bamfile, outfile, bed=False, map_quality=15):
     bam = pysam.AlignmentFile(bamfile, "rb")
     out = open(outfile, "w+")
     qual_num = 0
     qual_dnm = 0
-    nuc_map = {"a":0, "t":1, "g":2, "c":3, "A":0, "T":1, "G":2, "C":3}
+    nuc_map = {"a": 0, "t": 1, "g": 2, "c": 3, "A": 0, "T": 1, "G": 2, "C": 3}
+    allele_num = [0, 0, 0, 0]
 
     def gt_0(x):
         return x > 0
@@ -26,10 +25,13 @@ def get_biallelic_coverage(bamfile, outfile, bed = False, map_quality = 15):
         ATGC = [0, 0, 0, 0]
         bases = pcol.get_query_sequences()
         for b in bases:
-            if b == '' or b == 'N' or b == 'n': continue
+            if b == '' or b == 'N' or b == 'n':
+                continue
             ATGC[nuc_map[b]] += 1
         ATGC = list(filter(gt_0, ATGC))
-        if not len(ATGC) == 2:
+        a_num = len(ATGC)
+        allele_num[a_num] += 1
+        if not a_num == 2:
             return False
         out.write("%d %d\n" % (min(ATGC), sum(ATGC)))
         return True
@@ -38,14 +40,17 @@ def get_biallelic_coverage(bamfile, outfile, bed = False, map_quality = 15):
         f = open(bed)
         for line in f:
             br = line.split()
-            for pcol in bam.pileup(br[0], int(br[1]), int(br[2]), min_base_quality = map_quality):
+            for pcol in bam.pileup(br[0], int(br[1]), int(br[2]),
+                                   min_base_quality=map_quality):
                 if pcol_iter(pcol):
-                    qual_num += sum(map(lambda x: 10 ** -(x/10), pcol.get_query_qualities()))
+                    qual_num += sum(map(lambda x: 10 ** -(x/10),
+                                        pcol.get_query_qualities()))
                     qual_dnm += pcol.get_num_aligned()
     else:
-        for pcol in bam.pileup(min_base_quality = map_quality):
+        for pcol in bam.pileup(min_base_quality=map_quality):
             if pcol_iter(pcol):
-                qual_num += sum(map(lambda x: 10 ** -(x/10), pcol.get_query_qualities()))
+                qual_num += sum(map(lambda x: 10 ** -(x/10),
+                                    pcol.get_query_qualities()))
                 qual_dnm += pcol.get_num_aligned()
     print("Base quality error probability:")
     print(qual_num/qual_dnm)
@@ -64,16 +69,17 @@ def denoise_reads(readfile, p_err):
 
     reads = np.loadtxt(readfile)
     original_len = len(reads)
-    x = reads[:,0]
-    error_model = stats.binom(np.mean(reads[:,1]), p_err)
+    x = reads[:, 0]
+    error_model = stats.binom(np.mean(reads[:, 1]), p_err)
     em_lh = error_model.pmf(x)
     em_lh[em_lh < EPS] = EPS  # replace 0s with EPS
     # set prior values
-    nm_mean = np.mean(x[np.random.randint(len(x), size = 100)])
-    nm_std = np.std(x[np.random.randint(len(x), size = 100)])
-    nm_lh_old = np.ones_like(x) * EPS  # initial old value assumes 0 probability
+    nm_mean = np.mean(x[np.random.randint(len(x), size=100)])
+    nm_std = np.std(x[np.random.randint(len(x), size=100)])
+    # initial old value assumes 0 probability
+    nm_lh_old = np.ones_like(x) * EPS
     nm_lh = stats.norm.pdf(x, nm_mean, nm_std)
-    nm_lh[nm_lh <  EPS] = EPS  # replace 0s with EPS
+    nm_lh[nm_lh < EPS] = EPS  # replace 0s with EPS
     posterior = None
 
     iters = 0
@@ -83,14 +89,16 @@ def denoise_reads(readfile, p_err):
         iters += 1
         posterior = nm_lh / (nm_lh + em_lh)
         nm_mean = np.dot(posterior, x) / np.sum(posterior)
-        nm_std = np.sqrt(np.dot(posterior, (x - nm_mean) ** 2) / np.sum(posterior))
+        nm_std = np.sqrt(np.dot(posterior, (x - nm_mean) ** 2) /
+                         np.sum(posterior))
         nm_lh_old = nm_lh
         nm_lh = stats.norm.pdf(x, nm_mean, nm_std)
-        nm_lh[nm_lh <  EPS] = EPS  # replace 0s with EPS
+        nm_lh[nm_lh < EPS] = EPS  # replace 0s with EPS
     print(log_lh(nm_lh))
 
     print("Performed %s iterations" % iters)
     print(nm_mean, nm_std)
     print("")
-    print("%s percent of data removed" % ((1 - (sum(posterior == 1)/original_len)) * 100))
+    print("%s percent of data removed" % (
+        (1 - (sum(posterior == 1)/original_len)) * 100))
     return posterior, reads[posterior == 1]
