deee4fc401a6a31486cceae07c44065f05f753b8
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2020-05-18 14:23:38 -0400

    added options for system library and compiling htslib

--- setup.py
+++ setup.py
@@ -1,17 +1,60 @@
 import setuptools
+import subprocess
+import os
 from Cython.Build import cythonize
 from distutils.extension import Extension
+
+
+def is_library_installed(lib):
+    o = subprocess.run(['whereis %s' % lib], stdout=subprocess.PIPE,
+                       shell=True)
+    if o.returncode != 0:
+        raise Exception("""whereis command not found. Unable to determine if
+                        library %s is present""" % lib)
+    return not (o.stdout[len(lib)+1:] == b"\n")
+
+
+def install_htslib():
+    print("Compiling C htslib library")
+    old_dir = os.getcwd()
+    os.chdir("htslib")  # change into htslib directory
+
+    def run_cmd_raise_if_error(cmd):
+        o = subprocess.run([cmd], shell=True)
+        if o.returncode != 0:
+            raise Exception("An error occured when running '%s' command" % cmd)
+    # run compilation commands
+    run_cmd_raise_if_error("autoheader")
+    run_cmd_raise_if_error("autoconf")
+    run_cmd_raise_if_error("./configure")
+    run_cmd_raise_if_error("make")
+    os.chdir(old_dir)
+    print("\tsuccess!")
+
+
+source_files = ['PloidPy/process_bam.pyx', 'PloidPy/parse_bam.c']
+libraries = ["m"]
+include_dirs = []
+headers = ["PloidPy/parse_bam.h"]
+
+
+if is_library_installed("libhts"):
+    print("""htslib library found in system, using system version in
+          installation.""")
+    libraries.append("hts")
+else:
+    print("htslib library not found in system.")
+    install_htslib()
+    include_dirs.append("htslib")
+    libraries.append("htslib/hts")
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extensions = [
-    Extension(
-       'PloidPy.process_bam', ['PloidPy/process_bam.pyx',
-                               'PloidPy/parse_bam.c'],
-        libraries=["m"],
-    )
+    Extension('PloidPy.process_bam', source_files, libraries=libraries,
+              include_dirs=include_dirs)
 ]
 
 setuptools.setup(
@@ -37,6 +80,6 @@ setuptools.setup(
     ],
     scripts=['scripts/PloidPy'],
     python_requires='>=3.6',
-    headers=["PloidPy/parse_bam.h"],
+    headers=headers,
     ext_modules=cythonize(extensions)
 )
