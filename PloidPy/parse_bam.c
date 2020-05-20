d186758b50793e4e72c6194951c39b089013557f
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2020-05-20 12:02:42 -0400

    added cigar string functionality

--- PloidPy/parse_bam.c
+++ PloidPy/parse_bam.c
@@ -20,28 +20,42 @@ void cmap(char *bam, samFile *bamfile, char *chrom, int start, int end,
 	while(sam_itr_next(bamfile, itr, read) > 0) {
 		
 		int32_t pos = read->core.pos;
-		uint32_t len = read->core.l_qseq;
+		int read_pos = 0;
+		int ref_pos = 0;
+		uint32_t cigar_len = read->core.n_cigar;
 		
 		uint8_t *q = bam_get_seq(read);
 		uint32_t q2 = read->core.qual;
 		if ( min_mapq > q2 ) { continue; }
 		if (read->core.flag & (BAM_FUNMAP | BAM_FDUP | BAM_FSECONDARY | BAM_FQCFAIL)) { continue; }
-			
-		char *qseq = (char *)malloc(len);
+		uint32_t *cigar = bam_get_cigar(read);
 		int i;
-		for(i=0; i < len ; i++){
-			qseq[i] = seq_nt16_str[bam_seqi(q,i)];
-			if ( (pos + i) > end || (pos + i) < start) { continue; }
-			if ( min_bseq > q[i] ) { continue; }
-			n = n + pow(10, - q[i] / 10);
-			d = d + 1;
-			if (qseq[i] == 'A') { count_mat[(((pos+i) - start)* ROWSTRIDE)] += 1; }
-			if (qseq[i] == 'C') { count_mat[(((pos+i) - start)* ROWSTRIDE) + 1] += 1; }
-			if (qseq[i] == 'G') { count_mat[(((pos+i) - start)* ROWSTRIDE) + 2] += 1; }
-			if (qseq[i] == 'T') { count_mat[(((pos+i) - start)* ROWSTRIDE) + 3] += 1; }
+		for(i=0; i < cigar_len ; i++) {
+			int op = cigar[i] & BAM_CIGAR_MASK;
+			int l = cigar[i] >> BAM_CIGAR_SHIFT;
+			if (op  == BAM_CMATCH || op == BAM_CEQUAL || op == BAM_CDIFF) {
+				int j;
+				for(j = 0; j < l; j++) {
+					int read_p = read_pos;
+					int ref_p = ref_pos;
+					read_pos += 1;
+					ref_pos += 1;
+					if ( (pos + ref_p) > end || (pos + ref_p) < start) { continue; }
+					if ( min_bseq > q[read_p] ) { continue; }
+					char base = (char) seq_nt16_str[bam_seqi(q, read_p)];
+					n = n + pow(10, - q[read_p] / 10);
+					d = d + 1;
+					if (base == 'A') { count_mat[(((pos + ref_p) - start) * ROWSTRIDE)] += 1; }
+					if (base == 'C') { count_mat[(((pos + ref_p) - start) * ROWSTRIDE) + 1] += 1; }
+					if (base == 'G') { count_mat[(((pos + ref_p) - start) * ROWSTRIDE) + 2] += 1; }
+					if (base == 'T') { count_mat[(((pos + ref_p) - start) * ROWSTRIDE) + 3] += 1; }
+				}
+			}
+			else if (op == BAM_CREF_SKIP || op == BAM_CDEL) { ref_pos += l; }
+			else if (op == BAM_CINS || op == BAM_CSOFT_CLIP) { read_pos += l; }
 		}
 	}
-    
+	
 	*numer = n;
 	*denom = d;
 	bam_destroy1(read);
--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ extensions = [
 
 setuptools.setup(
     name="PloidPy",
-    version="1.1.0",
+    version="1.1.1",
     author="Oluwatosin Olayinka",
     author_email="oaolayin@live.unc.edu",
     description="Discrete mixture model based ploidy inference tool",
